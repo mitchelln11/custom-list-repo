0a5d4b9b0cf45621f9ba15f215668601cc5d1cc5
mitchelln11 <mitchellnaleid@gmail.com>
2019-11-05 11:10:11 -0600

    create basic classes and tie references in to unit testing file

--- CustomListUnitTesting/CustomListUnitTesting/Class1.cs
+++ CustomListUnitTesting/CustomListUnitTesting/Class1.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CustomListUnitTesting
 {
-    class Class1
+    public class CustomClassList<T>
     {
+        private T[] items;
+        private int capacity;
+        private int count;
+
+        public int Count
+        {
+            get 
+            {
+                return count;    
+            }
+        }
+        public CustomClassList()
+        {
+            capacity = 4;
+            count = 0;
+            items = new T[capacity]; //Setting array inside CustomClassList
+        }
     }
 }
--- CustomListUnitTesting/UnitTestCustomList/UnitTest1.cs
+++ CustomListUnitTesting/UnitTestCustomList/UnitTest1.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListUnitTesting;
 
 namespace UnitTestCustomList
 {
     [TestClass]
-    public class UnitTest1
+    public class CustomListUnitTesting
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Custom_List_Adding_Values()
         {
+            //arrange
+            CustomClassList<int> intList1 = new CustomClassList<int>();
+            int expected = 1;
+            int actual;
+            int number1 = 5; // Reference in 
+
+            //act
+            actual = intList1.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
